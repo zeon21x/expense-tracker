06e1677cf1eab66fa2f8d636e5c6fa760067a036
kaif khan <Kaifkhan660598@gmail.com>
2026-02-27 04:33:43 +0530

    fixed allowed hosts

--- ExpenseTracker/settings.py
+++ ExpenseTracker/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '4--(d0^o%3vqt#-c(hf+8)a$95z8gbo57xol5pft!%xpve9_zd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
